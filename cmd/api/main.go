a368d341c09684550e0db29b931f0d225553a591
reza <ahmad.reza.musthafa2@gmail.com>
2025-10-18 10:03:41 +0700

    create database and redis docker compose

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"github.com/ahmadrezamusthafa/ice-todo-service/config"
+	"github.com/ahmadrezamusthafa/ice-todo-service/infrastructure/database"
 	"github.com/ahmadrezamusthafa/ice-todo-service/infrastructure/logger"
 )
 
@@ -12,4 +14,20 @@ func main() {
 	if err := cfg.ValidateConfig(); err != nil {
 		log.Fatal("Configuration error: %v", err)
 	}
+
+	mysqlConnector := database.NewMySQLConnector(cfg, log)
+	db, err := mysqlConnector.Connect()
+	if err != nil {
+		log.Fatal("Failed to connect to MySQL: %v", err)
+	}
+	defer mysqlConnector.Close()
+
+	redisConnector := database.NewRedisConnector(cfg, log)
+	redisClient, err := redisConnector.Connect()
+	if err != nil {
+		log.Fatal("Failed to connect to Redis: %v", err)
+	}
+	defer redisConnector.Close()
+
+	fmt.Println(db, redisClient)
 }
